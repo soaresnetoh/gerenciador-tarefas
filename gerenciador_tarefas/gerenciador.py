0fec5f363c5cfdfad18ba27f4bb557c35b1da2f3
Hernani Soares <hernani.soares@ramper.com.br>
2022-05-19 19:48:42 -0300

    fix

--- gerenciador_tarefas/gerenciador.py
+++ gerenciador_tarefas/gerenciador.py
@@ -1,34 +1,26 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from enum import Enum
+from uuid import UUID, uuid4
+
+from fastapi import FastAPI, status
 from pydantic import BaseModel, constr
 
-# TAREFAS = [
-#     {
-#         "id": "1",
-#         "titulo": "fazer compras",
-#         "descrição": "comprar leite e ovos",
-#         "estado": "não finalizado",
-#     },
-#     {
-#         "id": "2",
-#         "titulo": "levar o cachorro para tosar",
-#         "descrição": "está muito peludo",
-#         "estado": "não finalizado",
-#     },
-#     {
-#         "id": "3",
-#         "titulo": "lavar roupas",
-#         "descrição": "estão sujas",
-#         "estado": "não finalizado",
-#     },
-# ]
-
 app = FastAPI()
-class Tarefa(BaseModel):
-    titulo: str
 
-class Tarefa(BaseModel):
+
+class EstadosPossiveis(str, Enum):
+    finalizado = "finalizado"
+    nao_finalizado = "não finalizado"
+
+
+class TarefaEntrada(BaseModel):
     titulo: constr(min_length=3, max_length=50)
+    descricao: constr(max_length=140)
+    estado: EstadosPossiveis = EstadosPossiveis.nao_finalizado
+
+
+class Tarefa(TarefaEntrada):
+    id: UUID
+
 
 TAREFAS = []
 
@@ -37,11 +29,12 @@ TAREFAS = []
 def listar():
     return TAREFAS
 
-@app.post('/tarefas')
-def criar():
-    pass
 
-@app.post('/tarefas')
-def criar(tarefa: Tarefa):
-    pass
-
+@app.post(
+    "/tarefas", response_model=Tarefa, status_code=status.HTTP_201_CREATED
+)
+def criar(tarefa: TarefaEntrada):
+    nova_tarefa = tarefa.dict()
+    nova_tarefa.update({"id": uuid4()})
+    TAREFAS.append(nova_tarefa)
+    return nova_tarefa
